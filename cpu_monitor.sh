3cde300c73e33815b16bee3ac2e076578a1a1bec
maximofn <maximofn@gmail.com>
2024-05-03 09:19:07 +0200

    Create logs files

--- cpu_monitor.sh
+++ cpu_monitor.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 # Get script path
 SCRIPT_PATH=$(dirname $0)
-/usr/bin/python3 $SCRIPT_PATH/cpu_monitor.py
+/usr/bin/python3 $SCRIPT_PATH/cpu_monitor.py >cpu_monitor.log 2>cpu_monitor_error.log
